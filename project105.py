a47ec6dda8750c557b9cdda581d63a49dcf09527
preetitanishka <71656803+preetitanishka@users.noreply.github.com>
2021-09-04 11:45:58 +0530

    Add files via upload

---
+++ project105.py
@@ -0,0 +1,33 @@
+import math
+import csv
+with open('data.csv',newline='')as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+
+data=file_data.pop(0)
+# finding mean 
+def mean(data): 
+   n= len(data) 
+   total =0 
+   for x in data: 
+      total += int(x) 
+   mean = total / n 
+   return mean
+
+squared_list=[]
+for number in data:
+    a=int(number)-mean(data)
+    a=a**2
+    squared_list.append(a)
+
+    
+    #getting sum 
+sum =0 
+for i in squared_list: 
+    sum =sum + i 
+     #dividing the sum by the total values 
+result = sum/ (len(data)-1) 
+     # getting the deviation by taking square root of the result 
+std_deviation = math.sqrt(result) 
+print(std_deviation) 
+     #print("derived using predefined function ",statistics.stdev(data))
